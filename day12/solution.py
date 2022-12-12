58a28e8c5b03f7da9c9998d10359f1602f3325ae
Steinar E <s.r.eiksen@gmail.com>
2022-12-12 22:07:45 +0100

    Updated day 4

--- day12/solution.py
+++ day12/solution.py
@@ -26,14 +26,64 @@ def solve(part=1, use_sample_data=True):
             return 1
         return (all_characters.index(target)-all_characters.index(curr))<=1
 
+    def breadth_first_search(starting):
+        q=[]
+        visited = {}
+        levels = {}
+        visited[starting]=starting
+        levels[starting]=1
+        q.append(starting)
+        qmap={}
+        qmap[starting]=starting
+        while len(q)>0:
+            node = q.pop(0)
+            current_cell = df.iloc[node[0], node[1]]
+            if current_cell == "z":
+                print("Found E", levels[node])
+                found_solutions.append(levels[node])
+                qmap.pop(node)
+                continue
+
+            check1=(node[0] + 1, node[1])
+            if (node[0] + 1) < rows and  not check1 in qmap:
+                next_cell = df.iloc[node[0]+1, node[1]]
+                if calc_diff_ok(current_cell, next_cell):
+                    levels[check1] = levels[node] + 1
+                    q.append(check1)
+                    qmap[check1]=check1
+
+            check2 = (node[0] - 1, node[1])
+            if (node[0] - 1) >= 0 and not check2  in qmap:#in visited:
+                next_cell = df.iloc[node[0]- 1, node[1]]
+                if calc_diff_ok(current_cell, next_cell):
+                    levels[check2] = levels[node] + 1
+                    q.append(check2)
+                    qmap[check2] = check2
+
+            check3 = (node[0], node[1] + 1)
+            if (node[1] + 1) < cols and not check3 in qmap:#visited:
+                next_cell = df.iloc[node[0], node[1]+1]
+                if calc_diff_ok(current_cell, next_cell):
+                    levels[check3] = levels[node] + 1
+                    q.append(check3)
+                    qmap[check3] = check3
+
+            check4 = (node[0], node[1] - 1)
+            if (node[1] - 1) >= 0 and not check4 in qmap:#visited:
+                next_cell = df.iloc[node[0], node[1]-1]
+                if calc_diff_ok(current_cell, next_cell):
+                    levels[check4] = levels[node] + 1
+                    q.append(check4)
+                    qmap[check4] = check4
+
+            #qmap.pop(node)
+
     def move(i,j, steps,visited={}):
         if len(found_solutions)>0 and steps>min(found_solutions):
             return
         visited[(i,j)]=(i,j)
         current_cell = df.iloc[i, j]
-        #prevchar=path[-1:]
-        #path=path+"." + current_cell
-        if current_cell=="E" :
+        if current_cell=="z" :
             print("Found E", steps)
             print(path)
             found_solutions.append(steps)
@@ -75,7 +125,16 @@ def solve(part=1, use_sample_data=True):
 
 
     path=""
-    move(0,0,1, {})
-    print(found_solutions)
+    #move(0,0,1, {})
+    if part==1:
+        breadth_first_search((0,0))
+    else:
+        for i in range(df.shape[0]):  # iterate over rows
+            for j in range(df.shape[1]):  # iterate over columns
+                current_cell = df.iloc[i, j]
+                if current_cell=="a":
+                    print("Staing from ",(i, j))
+                    breadth_first_search((i, j))
 
+    print(found_solutions)
     print(min(found_solutions))
--- main.py
+++ main.py
@@ -9,4 +9,4 @@ from day10.solution import solve as solve10
 from day11.solution import solve as solve11
 from day12.solution import solve as solve12
 if __name__ == '__main__':
-    solve12(1, False)
+    solve12(2, False)
